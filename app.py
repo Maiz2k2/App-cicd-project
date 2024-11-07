1e6cff2aa169f1cc77678ce968ba0134fe5a1b74
Maiz2k2 <75028660+Maiz2k2@users.noreply.github.com>
2024-11-07 18:40:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class hello:
     def GET(self, name):
         if not name:
             name = 'World'
-        return 'Hello CICD was done by Maiz and Mazin, This Project also uses Docker and Docker compose, ' + name + '!'
+        return 'Hello CICD was done by Maiz and Mazin, This Project also Docker and Docker compose, ' + name + '!'
 
 if __name__ == "__main__":
     app.run()
